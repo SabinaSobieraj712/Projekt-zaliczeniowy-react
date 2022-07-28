47a21107d093a5e3e6f11fc089e486ac282a47f4
SabinaSobieraj712 <SabinaSobieraj712@gmail.com>
2022-07-28 18:37:27 +0200

    React first project

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,6 +4,7 @@ import "../style/style.css";
 const textOne = "oferta";
 const textTwo = "o nas";
 const textThree = "kontakt";
+const textFour = "moja firma";
 
 const hiperlinkSet = (className, link, linkText ) => {
   return (<a className={className} href={link}>{linkText}</a>)
@@ -19,7 +20,7 @@ export const NavigationBar = () => {
   return (
     <nav>
       <div className="navigation-container container">
-        <p className="navigation_container-p">web driver</p>
+        <p className="navigation_container-p">{textFour}</p>
         <ul className="navigation_container-link">
           <li>
               {hiperlinkSet("link-us", "#work", textOne )}
